ed3ac3b84c664842046027faa1ffbf5089eca953
Adon Metcalfe <adonm@fastmail.fm>
2019-12-06 07:11:26 +0800

    blob upload

--- meraki-clients/handler.py
+++ meraki-clients/handler.py
@@ -1,4 +1,5 @@
 from meraki import meraki
+from azure.storage.blob import BlobServiceClient
 import json
 
 def handle(req):
@@ -6,19 +7,27 @@ def handle(req):
     Args:
         req (str): request body
     """
-    apikey = open("/var/openfaas/secrets/meraki-api-key").read().strip()
-
-    mOrgs = meraki.myorgaccess(apikey)
+    secrets = dict()
     devices = list()
     clients = list()
+    for key in ["meraki-api-key", "blob-connection-string", "meraki-blob-container"]:
+        secrets[key] = open("/var/openfaas/secrets/" + key).read().strip()
+
+    mOrgs = meraki.myorgaccess(secrets["meraki-api-key"])
 
     for org in mOrgs:
-        for net in meraki.getnetworklist(apikey, org["id"]):
-            devices += meraki.getnetworkdevices(apikey, net["id"])
+        for net in meraki.getnetworklist(secrets["meraki-api-key"], org["id"]):
+            devices += meraki.getnetworkdevices(secrets["meraki-api-key"], net["id"])
 
     for device in devices:
-        clients += meraki.getclients(apikey, device["serial"])
+        clients += meraki.getclients(secrets["meraki-api-key"], device["serial"])
 
-    return json.dumps(clients)
+    blobclient = BlobServiceClient.from_connection_string(secrets["blob-connection-string"])
+    container = blobclient.get_container_client(secrets["meraki-blob-container"])
+    blobname = "clients-{}.json".format(datetime.date.today().isoformat())
+    container.upload_blob(blobname, data=json.dumps(clients, indent=2))
+
+    response = "Uploaded {} containing {} clients".format(blobname, len(clients))
+    return response
 
 
