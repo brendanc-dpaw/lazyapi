1cda8c283f96b6bba6cea694178dcd05ab5be249
Adon Metcalfe <adonm@fastmail.fm>
2020-01-15 16:33:55 +0800

    data harvest bits

--- functions/agol_unlicense.py
+++ functions/agol_unlicense.py
@@ -3,8 +3,19 @@ from lazyapi import app
 import os
 import json, datetime
 from arcgis.gis import GIS
+from collections import defaultdict
+
+def loadGIS():
+    return GIS(username=os.getenv("AGOL_USERNAME"), password=os.getenv("AGOL_PASSWORD"), url=os.getenv("AGOL_URL"))
 
 @app.get("/esri/agol-unlicense")
 def agolunlicense():
-    gis = GIS(username=os.getenv("AGOL_USERNAME"), password=os.getenv("AGOL_PASSWORD"), url=os.getenv("AGOL_URL"))
-    return gis
+    gis = loadGIS()
+    content = defaultdict(list)
+    for item in gis.content.search('', max_items=10000):
+        content[item.owner].append(item)
+    users = gis.users.search()
+    for user in users:
+        if user.username not in content.keys():
+            print("may unlicense {}, lastlogin {}".format(user.username, user.lastLogin))
+    return locals()
